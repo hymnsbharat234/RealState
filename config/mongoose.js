a0f2d6691810afb5aff8fd2b228752735652f04c
“hymnsbharat234” <chandrakantyadav814101@gmail.com>
2020-08-12 21:35:26 +0530

    updated today file

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/codeial_development');
+mongoose.connect('mongodb://localhost/srconstruction');
 const db = mongoose.connection;
 
-db.on('error',console.error.bind(console,'Error in creating database'));
+db.on('error', console.error.bind(console, 'Error in creating database'));
 
-db.once('open',function()
-{
+db.once('open', function() {
     console.log('Database succesfully created');
 });
 
-module.exports = db; 
+module.exports = db;
